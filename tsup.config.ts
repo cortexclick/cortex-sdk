43df9deabefef44b872369ba38c1eb90103ac73e
Jeremy Moseley <jeremy@cortexclick.com>
2024-07-23 20:39:54 -0700

    Linting.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  target: "node",
   dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
   sourcemap: true,
   clean: true,
 });
