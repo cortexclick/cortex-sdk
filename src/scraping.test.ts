13bf0954fa43efed771962ae1bac2c17f35af648
chaosrealm <eugene@cortexclick.com>
2024-12-17 16:23:13 -0800

    Linting

--- src/scraping.test.ts
+++ src/scraping.test.ts
@@ -12,55 +12,94 @@ function getRandomCatalogName(): string {
   return `sdk-scraper-test-${Date.now()}-${Math.floor(Math.random() * 1000)}`;
 }
 
-suite.skipIf(process.env.RUN_SCRAPER_TESTS !== "true")(
-  "scraping tests",
-  () => {
-    test("Test scraping single URL", { timeout: 60000 }, async () => {
-      const catalogName = getRandomCatalogName();
+suite.skipIf(process.env.RUN_SCRAPER_TESTS !== "true")("scraping tests", () => {
+  test("Test scraping single URL", { timeout: 60000 }, async () => {
+    const catalogName = getRandomCatalogName();
 
-      const config: CatalogConfig = {
-        description: "foo bar",
-        instructions: ["a", "b"],
-      };
+    const config: CatalogConfig = {
+      description: "foo bar",
+      instructions: ["a", "b"],
+    };
 
-      const catalog = await testClient.configureCatalog(catalogName, config);
+    const catalog = await testClient.configureCatalog(catalogName, config);
 
-      const docs: UrlDocument[] = [
-        {
-          url: "https://www.cortexclick.com/",
-          contentType: "url",
-        },
-      ];
+    const docs: UrlDocument[] = [
+      {
+        url: "https://www.cortexclick.com/",
+        contentType: "url",
+      },
+    ];
 
-      await catalog.upsertDocuments(docs);
+    await catalog.upsertDocuments(docs);
 
-      let docsFound = false;
+    let docsFound = false;
 
-      while (!docsFound) {
-        const docCount = await catalog.documentCount();
-        if (docCount === 1) {
-          docsFound = true;
-        } else {
-          console.log("no docs found. sleeping...");
-          await setTimeout(5000);
-        }
-      }
-
+    while (!docsFound) {
       const docCount = await catalog.documentCount();
-      expect(docCount).toBe(1);
+      if (docCount === 1) {
+        docsFound = true;
+      } else {
+        console.log("no docs found. sleeping...");
+        await setTimeout(5000);
+      }
+    }
 
-      await catalog.delete();
-    });
+    const docCount = await catalog.documentCount();
+    expect(docCount).toBe(1);
 
-    test("Test scraping sitemap", { timeout: 120000 }, async () => {
-      const catalogName = getRandomCatalogName();
+    await catalog.delete();
+  });
+
+  test("Test scraping sitemap", { timeout: 120000 }, async () => {
+    const catalogName = getRandomCatalogName();
+
+    const config: CatalogConfig = {
+      description: "foo bar",
+      instructions: ["a", "b"],
+    };
+
+    const catalog = await testClient.configureCatalog(catalogName, config);
+
+    const docs: SitemapDocument[] = [
+      {
+        sitemapUrl: "https://www.cortexclick.com/sitemap.xml",
+        contentType: "sitemap-url",
+      },
+    ];
+
+    await catalog.upsertDocuments(docs);
+
+    let docsFound = false;
+
+    while (!docsFound) {
+      const docCount = await catalog.documentCount();
+      if (docCount >= minExpectedSitemapUrls) {
+        docsFound = true;
+      } else {
+        console.log(`${docCount} docs found. sleeping...`);
+        await setTimeout(5000);
+      }
+    }
+
+    await catalog.delete();
+  });
+
+  test(
+    "Test isolation of scraping multiple catalogs at once",
+    { timeout: 120000 },
+    async () => {
+      const catalogName1 = getRandomCatalogName();
+      const catalogName2 = getRandomCatalogName();
+      const catalogName3 = getRandomCatalogName();
 
       const config: CatalogConfig = {
         description: "foo bar",
         instructions: ["a", "b"],
       };
 
-      const catalog = await testClient.configureCatalog(catalogName, config);
+      const catalog1 = await testClient.configureCatalog(catalogName1, config);
+      const catalog2 = await testClient.configureCatalog(catalogName2, config);
+      const catalog3 = await testClient.configureCatalog(catalogName3, config);
 
       const docs: SitemapDocument[] = [
         {
@@ -69,82 +108,31 @@ suite.skipIf(process.env.RUN_SCRAPER_TESTS !== "true")(
         },
       ];
 
-      await catalog.upsertDocuments(docs);
+      catalog1.upsertDocuments(docs);
+      catalog2.upsertDocuments(docs);
+      catalog3.upsertDocuments(docs);
 
       let docsFound = false;
 
       while (!docsFound) {
-        const docCount = await catalog.documentCount();
-        if (docCount >= minExpectedSitemapUrls) {
+        const count1 = await catalog1.documentCount();
+        const count2 = await catalog2.documentCount();
+        const count3 = await catalog3.documentCount();
+        if (
+          [count1, count2, count3].every((e) => e >= minExpectedSitemapUrls)
+        ) {
           docsFound = true;
         } else {
-          console.log(`${docCount} docs found. sleeping...`);
+          console.log(
+            `Waiting for all 3 catalogs to be populated. C1: ${count1}, C2: ${count2}, C3: ${count3} docs found. sleeping...`,
+          );
           await setTimeout(5000);
         }
       }
 
-      await catalog.delete();
-    });
-
-    test(
-      "Test isolation of scraping multiple catalogs at once",
-      { timeout: 120000 },
-      async () => {
-        const catalogName1 = getRandomCatalogName();
-        const catalogName2 = getRandomCatalogName();
-        const catalogName3 = getRandomCatalogName();
-
-        const config: CatalogConfig = {
-          description: "foo bar",
-          instructions: ["a", "b"],
-        };
-
-        const catalog1 = await testClient.configureCatalog(
-          catalogName1,
-          config,
-        );
-        const catalog2 = await testClient.configureCatalog(
-          catalogName2,
-          config,
-        );
-        const catalog3 = await testClient.configureCatalog(
-          catalogName3,
-          config,
-        );
-
-        const docs: SitemapDocument[] = [
-          {
-            sitemapUrl: "https://www.cortexclick.com/sitemap.xml",
-            contentType: "sitemap-url",
-          },
-        ];
-
-        catalog1.upsertDocuments(docs);
-        catalog2.upsertDocuments(docs);
-        catalog3.upsertDocuments(docs);
-
-        let docsFound = false;
-
-        while (!docsFound) {
-          const count1 = await catalog1.documentCount();
-          const count2 = await catalog2.documentCount();
-          const count3 = await catalog3.documentCount();
-          if (
-            [count1, count2, count3].every((e) => e >= minExpectedSitemapUrls)
-          ) {
-            docsFound = true;
-          } else {
-            console.log(
-              `Waiting for all 3 catalogs to be populated. C1: ${count1}, C2: ${count2}, C3: ${count3} docs found. sleeping...`,
-            );
-            await setTimeout(5000);
-          }
-        }
-
-        await catalog1.delete();
-        await catalog2.delete();
-        await catalog3.delete();
-      },
-    );
-  },
-);
+      await catalog1.delete();
+      await catalog2.delete();
+      await catalog3.delete();
+    },
+  );
+});
